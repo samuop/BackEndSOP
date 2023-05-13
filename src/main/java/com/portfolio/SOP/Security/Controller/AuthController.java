b31163768ad62d37c30ab65ca7be37bfeb089bb1
sam215utn <samuelocta215@gmail.com>
2023-05-13 12:41:20 -0300

    .

--- src/main/java/com/portfolio/SOP/Security/Controller/AuthController.java
+++ src/main/java/com/portfolio/SOP/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = {"https://frontendSOP.web.app","http://localhost:4200"})
+@CrossOrigin(origins = {"https://frondsop.web.app","http://localhost:4200"})
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
